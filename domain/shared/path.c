cfccf08c5bf5255d681c67b1aa2bba659b9aaf83
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2024-03-09 12:24:30 -0300

    add dirname to get the o path

---
+++ domain/shared/path.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "path.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#elif __linux__
+    #include <unistd.h>
+    #include <limits.h>
+#endif
+
+char* dirname(const char* path) {
+
+    static char fullPath[PATH_MAX];
+
+    #ifdef _WIN32
+        if (!GetFullPathNameA(path, PATH_MAX, fullPath, NULL)) {
+            printf("Error getting full path\n");
+            return NULL;
+        }
+    #elif __linux__
+        if (realpath(nomeArquivo, caminhoCompleto) == NULL) {
+            perror("Error getting full path");
+            return NULL;
+        }
+    #else
+        #error "Unsupported platform"
+    #endif
+
+    return fullPath;
+}
